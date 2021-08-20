41e82bb533675b819259ab51d0e3a41835c708d6
Oyvind Hansen <oyvind.hansen@outlook.com>
2021-08-20 17:55:08 +0000

    log

--- storage.js
+++ storage.js
@@ -1,27 +1,29 @@
 const fs = require('fs');
 
-function write(path, barometer) {
+function write(dir, barometer) {
     let barometerData = barometer.getAll();
 
 	let barometerDataJSON = JSON.stringify(barometerData);
 
-	fs.writeFile(getFilePath(path), barometerDataJSON, (err) => {
+	fs.writeFile(getFilePath(dir), barometerDataJSON, 'utf-8', (err) => {
 		if (err) {
+			console.debug("An error occured while trying to write barometer data to file");
 			throw err;
 		}
 		console.debug("Barometer data saved.");
 	});
 }
 
-function getFilePath(path) {
-	path = path + "/offline.json";
-	console.debug("path: " + path)
+function getFilePath(dir) {
+	let path = dir + "/offline.json";
+	console.debug("file path: " + path)
 	return path;
 }
 
-function read(path) {
-	return fs.readFile(getFilePath(path), 'utf-8', (err, data) => {
+function read(dir) {
+	return fs.readFile(getFilePath(dir), 'utf-8', (err, data) => {
 		if (err) {
+			console.debug("An error occured while trying to read  barometer data from file");
 			throw err;
 		}
 	
